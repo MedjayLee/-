5d3bbff54df953005939f73e4217f30baff11bdf
Bogdan <li.bogdan44@gmail.com>
2022-12-05 23:10:18 +0500

    last sections and scroll button

---
+++ javascript/index.js
@@ -0,0 +1,17 @@
+let scrollBtn = document.getElementById("scrollBtn")
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+}
+scrollBtn.addEventListener("click", topFunction)
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
